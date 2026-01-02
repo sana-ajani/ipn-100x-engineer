90c1276a8452f032b0807303d37cc70765c300f3
Sana Ajani <sajani6@gatech.edu>
2026-01-02 17:58:34 -0600

    fixed quotes issue

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
                 Enter your location to find nearby restaurants
               </p>
               <p className="text-gray-400 text-sm mt-2">
-                Try searching for "San Francisco" or "94102"
+                Try searching for &quot;San Francisco&quot; or &quot;94102&quot;
               </p>
             </div>
           )}
